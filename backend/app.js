20e7592e57ae70da274f42651e2ed4451e4ab919
Danpatpang <puri8467@gmail.com>
2019-05-21 16:05:41 +0900

    Update routes

--- backend/app.js
+++ backend/app.js
@@ -5,36 +5,38 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/order');
-var usersRouter = require('./routes/current_order');
+var adminRouter = require('./routes/current_order');
 
 var app = express();
+
+require('dotenv').config();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded({extended: false}))
+    .use(cookieParser())
+    .use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+    .use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+let router = express.Router();
+let DB = require('../models');
 
-/* 주문 사이트 */
+/* 주문 저장 */
 router.post('/order/menu', function (req, res, next) {
     let body = req.body;
-    res.json(body);
+    let list = "";
+    for (let i = 0; i < req.body.menus.length; i++) {
+        if (body.menus[i].count !== 0) {
+            list += body.menus[i].name + " " + body.menus[i].count + "\n";
+        }
+    }
+
+    let order = {};
+    order.table_num = body.tableNum;
+    order.menus = list;
+    order.total_price = body.totalPrice;
+
+    DB.addOrder((err, result) => {
+        if (err) {
+            res.json("fail");
+        }
+        console.log(result);
+        res.json("success");
+    }, order);
 });
 
 module.exports = router;
